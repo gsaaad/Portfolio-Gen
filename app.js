eb31f4cdd0ebff2baf31d99c7a37437a07d1bfd7
George Saad <gsparqcode@gmail.com>
2022-04-25 18:27:36 -0400

    feature/updated-template

--- app.js
+++ app.js
@@ -1,46 +1,94 @@
-//using inquirer
+const fs = require("fs");
 const inquirer = require("inquirer");
-// console.log(inquirer);
-// const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-// const generatePage = require("./src/page-template");
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => confirmAbout,
+    },
+  ]);
+};
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-//   console.log("Portfolio Complete! Checkout index.hrml to see the output!");
-// });
-
-//how to ask a question and console.log the answer!
-
-//asking about questions for another project
 const promptProject = (portfolioData) => {
-  // if there is not portfoliodata.projects, create a new empty array
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
   console.log(`
 =================
 Add a New Project
 =================
 `);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   return inquirer
     .prompt([
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project name!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project description!");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
         name: "languages",
-        message: "What did you build this project with? (Check all that apply)",
+        message: "What did you this project with? (Check all that apply)",
         choices: [
           "JavaScript",
           "HTML",
@@ -55,6 +103,14 @@ Add a New Project
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project GitHub link!");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
@@ -71,61 +127,24 @@ Add a New Project
     ])
     .then((projectData) => {
       portfolioData.projects.push(projectData);
-      // after the prompt. then, push project data to portfoliodata projects~!
       if (projectData.confirmAddProject) {
-        //if user said yes to confirm add another project at the end, prompt again and pass through data with it
-        //if we didn't pass through portfolio data, we would have erased the data from the past prompt (multi projects in this case)
         return promptProject(portfolioData);
       } else {
-        //if user answers no to add another project, just return the data~!
         return portfolioData;
       }
     });
 };
-//prompt user + chaining the different functions
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your Name?",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your name!");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your Github Username",
-    },
-    {
-      type: "confirm",
-      name: "confirmAbout",
-      message:
-        "Would you like to enter some information about yourself for an 'About' section?",
-      default: true,
-    },
-    {
-      type: "input",
-      name: "about",
-      message: "Provide some information about yourself",
-      when: ({ confirmAbout }) => {
-        if (confirmAbout) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-    },
-  ]);
-};
+
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
